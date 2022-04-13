0f5eb6eb034ec00ca2bce2b01dad33998845b8e4
ohotnikov.ivan <ohotnikov.ivan@e-queo.net>
2022-04-13 15:44:19 +0300

    fix groups api endpoints

--- src/V3/LearningProgram/ProgramApi.php
+++ src/V3/LearningProgram/ProgramApi.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 namespace Ekvio\Integration\Sdk\V3\LearningProgram;
 
 use Ekvio\Integration\Sdk\ApiException;
-use Ekvio\Integration\Sdk\V2\EqueoClient;
+use Ekvio\Integration\Sdk\V3\EqueoClient;
 
 /**
  * Class ProgramApi
