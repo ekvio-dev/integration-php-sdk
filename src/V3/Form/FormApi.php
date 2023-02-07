8fc2653541b705b9e9e7ef1bb4686de996678e78
antonov.daniil <antonov.daniil@e-queo.net>
2023-02-07 17:53:38 +0300

    Add forms api methods

---
+++ src/V3/Form/FormApi.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types=1);
+
+namespace Ekvio\Integration\Sdk\V3\Form;
+
+use Ekvio\Integration\Sdk\V3\EqueoClient;
+
+class FormApi implements Form
+{
+    private const FORM_AUTO_ASSIGNMENTS_SEARCH_ENDPOINT = '/v3/forms/auto-assignments/search';
+    private const FORM_AUTO_ASSIGNMENTS_CREATE_AND_UPDATE_ENDPOINT = '/v3/forms/auto-assignments';
+
+    private EqueoClient $client;
+
+    public function __construct(EqueoClient $client)
+    {
+        $this->client = $client;
+    }
+
+    public function searchAutoAssignments(FormSearchAutoAssignmentsCriteria $criteria): array
+    {
+        $response = $this->client->request(
+            $criteria->method(),
+            self::FORM_AUTO_ASSIGNMENTS_SEARCH_ENDPOINT,
+            $criteria->queryParams(),
+            $criteria->body()
+        );
+
+        return $response['data'];
+    }
+
+    public function createAutoAssignments(array $autoAssignments): array
+    {
+        $response = $this->client->deferredRequest(
+            'POST',
+            self::FORM_AUTO_ASSIGNMENTS_CREATE_AND_UPDATE_ENDPOINT,
+            [],
+            ['data' => $autoAssignments]
+        );
+
+        return $response['data'];
+    }
+
+    public function updateAutoAssignments(array $autoAssignments): array
+    {
+        $response = $this->client->deferredRequest(
+            'PUT',
+            self::FORM_AUTO_ASSIGNMENTS_CREATE_AND_UPDATE_ENDPOINT,
+            [],
+            ['data' => $autoAssignments]
+        );
+
+        return $response['data'];
+    }
+}
