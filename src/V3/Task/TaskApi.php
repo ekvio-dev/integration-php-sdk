fc01c770f547b36a02e6843d72f7e9a0b0706c31
antonov.daniil <antonov.daniil@e-queo.com>
2025-01-24 11:57:58 +0300

    Обновление методов работы с заданиями

--- src/V3/Task/TaskApi.php
+++ src/V3/Task/TaskApi.php
@@ -9,11 +9,14 @@ use Ekvio\Integration\Sdk\V3\EqueoClient;
 
 class TaskApi implements Task
 {
+    private const CHUNK = 500;
     private const TASKS_STATISTIC_ENDPOINT = '/v3/tasks/statistic';
     private const TASKS_FIELDS_SEARCH_ENDPOINT = '/v3/tasks/fields/search';
     private const TASK_STATUSES_UPDATE_ENDPOINT = '/v3/tasks/answers/statuses';
     private const TASK_GET_ENDPOINT = '/v3/tasks/search';
     private const TASK_ASSIGNMENT_PERSONAL = '/v3/tasks/assignments/personal';
+    private const TASK_CREATE_ENDPOINT = '/v3/tasks';
+    private const TASK_FIELD_CREATE_ENDPOINT = '/v3/tasks/fields';
 
     private EqueoClient $client;
 
@@ -93,5 +96,33 @@ class TaskApi implements Task
             $criteria->queryParams(),
             $criteria->body()
         );
+    }
+
+    public function createTasks(array $tasks): array
+    {
+        $data = [];
+        foreach (array_chunk($tasks, self::CHUNK, true) as $chunk) {
+            $response = $this->client->deferredRequest('POST', self::TASK_CREATE_ENDPOINT, [], [
+                'data' => $chunk
+            ]);
+
+            $data = array_merge($data, $response['data']);
+        }
+
+        return $data;
+    }
+
+    public function createTaskFields(array $taskFields): array
+    {
+        $data = [];
+        foreach (array_chunk($taskFields, self::CHUNK, true) as $chunk) {
+            $response = $this->client->deferredRequest('POST', self::TASK_FIELD_CREATE_ENDPOINT, [], [
+                'data' => $chunk
+            ]);
+
+            $data = array_merge($data, $response['data']);
+        }
+
+        return $data;
     }
 }
