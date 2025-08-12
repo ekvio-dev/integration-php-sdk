4d14e69cccf3b5c9a4dd66f3dbb5a034abbb603b
Vuylov Dmitry <vuylov.dmitry@e-queo.com>
2025-08-12 17:05:41 +0300

    Extend assignment search entity type

--- src/V3/Assignment/SearchAssignmentCriteria.php
+++ src/V3/Assignment/SearchAssignmentCriteria.php
@@ -12,7 +12,7 @@ use Webmozart\Assert\Assert;
 
 class SearchAssignmentCriteria extends Criteria
 {
-    private const SEARCH_ENTITY_TYPE = ['task', 'media', 'shop', 'info', 'learning_program', 'track', 'event', 'message'];
+    private const SEARCH_ENTITY_TYPE = ['task', 'media', 'shop', 'info', 'learning_program', 'track', 'event', 'message', 'badge', 'kpi'];
     private const TYPE = ['personal', 'group'];
     private string $entity;
     private function __construct(string $entity)
